647b2c84f7a820cb19b41f5c5a1dc6ebac69e1e4
chabiaknt <chabiaknt@gmail.com>
2023-08-11 02:52:37 +0530

    firebase

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDqraoCMRGzrUSK6dUMkZqwJOw3CTvD2gk",
+  authDomain: "netflix-2adfd.firebaseapp.com",
+  projectId: "netflix-2adfd",
+  storageBucket: "netflix-2adfd.appspot.com",
+  messagingSenderId: "1013869878260",
+  appId: "1:1013869878260:web:842a44a08c4e94069df7a1",
+  measurementId: "G-GYEF801X01"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
